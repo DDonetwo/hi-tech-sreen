a4636080ee2e4f3ad071ed653e4e0a6da247d5ec
DDonetwo <design@gtlamp.ru>
2024-10-16 16:29:34 +0300

    add: script

---
+++ asset/js/script.js
@@ -0,0 +1,15 @@
+// попап
+
+document.addEventListener('DOMContentLoaded', function () {
+    event.preventDefault();
+    
+    document.querySelectorAll('#openForm').forEach((item) => item.addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    }));
+
+    document.querySelectorAll('#close').forEach((item) => item.addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }));
+});
